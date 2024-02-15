37733bede67f00db4dcbe4a9d0fb08bc046e6bb8
Hyakusetufutou <Hyakusetufutou358@gmail.com>
2024-02-15 21:40:09 +0900

    useSelectorとuseDispatchを準備

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { useSelector } from "react-redux";
 import "./App.scss";
 import Sidebar from "./components/sidebar/Sidebar";
 import Chat from "./components/chat/Chat";
 import Login from "./components/login/Login";
+import { useAppSelector } from "./app/hooks";
 
 function App() {
-  // const user = useSelector((state) => state.user.user);
-  const user = null;
+  const user = useAppSelector((state) => state.user);
+  // const user = null;
 
   return (
     <div className="App">
